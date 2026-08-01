0bd29929200d31d9c6e975e024c8817caee48d02
Divya Kapse <divyakapse1976@gmail.com>
2026-08-01 12:11:29 +0530

    Delete frontend directory

--- frontend/src/assets/index.js
+++
@@ -1,3 +0,0 @@
-// Centralized Asset & Icon Exports
-export const APP_NAME = "TrafficVision AI";
-export const APP_TAGLINE = "Smart Traffic Prediction & Congestion Management";
--- frontend/src/components/common/RoadDetailsModal.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { RoadDetailsSidePanel } from './RoadDetailsSidePanel';
-
-export const RoadDetailsModal = (props) => {
-  return <RoadDetailsSidePanel {...props} />;
-};
-
-export default RoadDetailsModal;
